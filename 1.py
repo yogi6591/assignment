4ba74a4559b6875ef7d6f0c6d0cbc99d8e8da2b9
khushi ram <khushiram6591@gmail.com>
2018-06-14 11:51:48 +0530

    second commit

--- 1.py
+++ 1.py
@@ -1,1 +1,37 @@
-print("i am doing python")hvfsdvfubdldnfnsnf
+#print anything.
+print("hello world")
+print("I'm learning python\n")
+
+
+#concatenate two strings.
+s1='khushi_ram_'
+s2='6316036'
+s3=s1+s2
+print(s3,"\n")
+
+
+#Take the values of x,y,z from user and print their values.
+x=int(input("enter the value of x"))
+y=int(input("enter the value of y"))
+z=int(input("enter the value of z"))
+print("the values of x,y,z are")
+print("x=",x)
+print("y=",y)
+print("z=",z,"\n")
+
+
+#print "let get started" on screen.
+print("let get started\n")
+
+
+#print the values using placeholder
+s="Acadview_"
+course="python_"
+fees=5000
+print('%s%s%d'%(s,course,fees),"\n")
+
+
+#question
+name="Tony_Stark__"
+salary=1000000
+print('%s%d'%(name,salary))
